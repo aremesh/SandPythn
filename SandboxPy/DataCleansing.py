1e3b28796316ab20d24f96e988fd045c4042b092
Ramesh Arumugagounder <aremesh@gmail.com>
2018-05-24 22:39:10 -0400

    File Moddified

--- SandboxPy/DataCleansing.py
+++ SandboxPy/DataCleansing.py
@@ -1,2 +1,25 @@
+# import the pandas library
 #https://www.tutorialspoint.com/python/python_data_cleansing.htm
-#test
+import pandas as pd
+import numpy as np
+df = pd.DataFrame(np.random.randn(5, 3), index=['a', 'c', 'e', 'f','h'],columns=['one', 'two', 'three'])
+df = df.reindex(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'])
+print (df)
+
+#Check for Missing Values
+
+import pandas as pd
+import numpy as np
+df = pd.DataFrame(np.random.randn(5, 3), index=['a', 'c', 'e', 'f','h'],columns=['one', 'two', 'three'])
+df = df.reindex(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'])
+print ( df['one'].isnull())
+
+#Replace NaN with a Scalar Value
+import pandas as pd
+import numpy as np
+df = pd.DataFrame(np.random.randn(5, 3), index=['a', 'c', 'e', 'f','h'],columns=['one', 'two', 'three'])
+df = df.reindex(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'])
+print ( df['one'].isnull())
+print( df.fillna(0))
+print (df.fillna(method='pad'))
+print (df.dropna())
